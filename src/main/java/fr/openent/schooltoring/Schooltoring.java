4a21a0c7ab94ca4aa72d0b3107a9138260d564e0
LEDUNOIS Simon <simon.ledunois@cgi.com>
2018-05-16 11:08:55 +0200

    Initial commit

---
+++ src/main/java/fr/openent/schooltoring/Schooltoring.java
@@ -0,0 +1,12 @@
+package fr.openent.schooltoring;
+
+import org.entcore.common.http.BaseServer;
+
+public class Schooltoring extends BaseServer {
+
+	@Override
+	public void start() {
+		super.start();
+	}
+
+}
